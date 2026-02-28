390548986d69f4ebc1f6871754f064a7c4d79bf9
acierto-hub <angeloyooriacierto.scc@gmail.com>
2026-02-28 18:34:32 +0800

    Ass1

---
+++ Ass1.java
@@ -0,0 +1,6 @@
+SwingUtilities.invokeLater(() -> {
+    JFrame frame = new JFrame("My First Swing Window");
+    frame.setSize(400, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+});
